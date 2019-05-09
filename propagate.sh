d951d2153c5acb74d3f08ccc4f9abb8e6d5aedf8
keithagroves <keith.groves@jointheleague.org>
2019-05-08 19:02:55 -0700

    Update propagate.sh

--- propagate.sh
+++ propagate.sh
@@ -88,7 +88,7 @@ function dir_command {
 }
 echo "moving through directories"
 #This loop will go to each immediate child and execute dir_command
-find . -maxdepth 1 -type d \( ! -name . \) -name 'Level*' | while read dir; do
+find . -maxdepth 1 -type d \( ! -name . \) -name '*' | while read dir; do
    dir_command "$dir/"
    
 done
