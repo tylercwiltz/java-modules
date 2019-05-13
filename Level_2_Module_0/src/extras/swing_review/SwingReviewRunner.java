fcda355925ef881643d9fb583eab1559483c9ead
League Student <league@WTS-iMac-11.attlocal.net>
2019-05-13 16:29:15 -0700

    SwingReview Recipe for students who test directly into Level2

---
+++ Level_2_Module_0/src/extras/swing_review/SwingReviewRunner.java
@@ -0,0 +1,11 @@
+package swing_review;
+/*
+ *    Copyright (c) The League of Amazing Programmers 2013-2019
+ *    Level 2
+ */
+
+public class SwingReviewRunner {
+	public static void main(String[] args) throws Exception {
+		new SwingReview().run();
+	}
+}
